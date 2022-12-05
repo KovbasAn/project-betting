88f10dd2b64cba5f09ea274bc924e7418a6c8af5
kovba_san <kovbas2311@gmail.com>
2022-12-05 21:04:05 +0200

    finish js

--- script.js
+++ script.js
@@ -17,8 +17,27 @@ function handleResults() {
 
     // calculate points
     const points = calcPoints(result, bet)
+    pointCells[i].innerText = points
+  }
+}
 
+function generate() {
+  return Math.floor(Math.random() * 10)
+}
+
+function calcPoints(result, bet) {  
+  if (result === bet) {
+    return 5
   }
 
-
+  result = result.split(' : ')
+  bet = bet.split(' : ')
+  const resultSign = Math.sign(result[0] - result[1])
+  const betSign = Math.sign(bet[0] - bet[1])
+  
+  if (resultSign === betSign) {
+    return 2
+  }
+  
+  return 0  
 }
